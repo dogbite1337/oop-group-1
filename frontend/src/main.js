963d8d2d42378fe0003086433ddb6a9b20f47599
dogbite1337 <kocak_cc25@hotmail.com>
2021-12-21 17:59:45 +0100

    Adding Bootstrap to Project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store.js';
 import router from './router.js';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 createApp(App).use(store).use(router).mount('#app');
