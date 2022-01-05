8e799c41a49401103c23b259b3bfe369cd5ee47c
Yang Li <odielee@hotmail.com>
2022-01-06 00:47:10 +0100

    will now store up to 6 search histories in DB, older ones will be removed upon exceeding 6

--- backend/src/main/java/com/company/Repositories/searchHistoryRepository.java
+++ backend/src/main/java/com/company/Repositories/searchHistoryRepository.java
@@ -7,10 +7,19 @@ import com.company.utilities.Encrypter;
 import java.sql.*;
 import java.util.ArrayList;
 
+
 public class searchHistoryRepository {
     Connection con;
 
     public void registerSearchHistory(SearchHistory newSearchHistory) throws SQLException {
+
+        ArrayList<SearchHistory> searchHistories = findHistoryListByUserId(newSearchHistory.getUserId());
+        if(searchHistories.size() > 5){
+            removeExceededHistoryByUserId(newSearchHistory.getUserId(), searchHistories.get(4).getTime());
+        }
+
+
+
         try {
             try {
                 con = DriverManager.getConnection(
@@ -23,8 +32,56 @@ public class searchHistoryRepository {
                 registerSearchHistory.setString(2, newSearchHistory.getKeyWord());
                 registerSearchHistory.setString(3, newSearchHistory.getTime());
                 registerSearchHistory.executeUpdate();
-
             }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        con.close();
+
+    }
+
+    public ArrayList<SearchHistory> findHistoryListByUserId(Integer userId) throws SQLException {
+        ArrayList<SearchHistory> historyListOfUserDescendingByTime = new ArrayList<SearchHistory>();
+        try {
+            try {
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/kittykitty","root","root");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            Statement searchHistoryList = con.createStatement();
+            String query = "Select * from searchhistories Where userId = " + userId + " ORDER BY time DESC;";
+            ResultSet result = searchHistoryList.executeQuery(query);
+            while(result.next()){
+                SearchHistory searchHistory = new SearchHistory(result.getInt("userId"), result.getString("keyWord"), result.getString("time"));
+                historyListOfUserDescendingByTime.add(searchHistory);
+            }
+
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        con.close();
+        return historyListOfUserDescendingByTime;
+    }
+
+
+    public void removeExceededHistoryByUserId(Integer userId, String time) throws SQLException {
+        try {
+            try {
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/kittykitty","root","root");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            PreparedStatement deleteSearchHistory = con.prepareStatement("Delete from searchhistories Where userId = ? AND time < ?", Statement.RETURN_GENERATED_KEYS);
+            deleteSearchHistory.setInt(1, userId);
+            deleteSearchHistory.setString(2, time);
+            deleteSearchHistory.executeUpdate();
+        }
+
         catch (SQLException ex) {
             ex.printStackTrace();
         }
